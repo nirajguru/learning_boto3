bc54344e49ff668c3943fa5feffa505c8004715f
Niraj Gurupanchayan <niraj.guru@gmail.com>
2022-06-23 21:55:14 +0100

    python program to get your AWS account ID

---
+++ get_account_id.py
@@ -0,0 +1,7 @@
+'''calling sts to get caller identity '''
+import profile
+import boto3
+
+sts = boto3.client('sts')
+aws_acount_id = sts.get_caller_identity().get('Account')
+print(f"Your AWS Account ID is {aws_acount_id}")
